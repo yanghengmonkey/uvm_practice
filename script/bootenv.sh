2e79aea63ecaee2299e135ae95685698d5ec43b0
hengyang <hengyang@iluvatar.ai>
2018-09-25 23:53:48 +0800

    FIX: test

--- script/bootenv.sh
+++ script/bootenv.sh
@@ -1,4 +1,5 @@
 #!/usr/sh
+rm -rf $(git rev-parse --show-toplevel)/.git/hooks && ln -snf $(git rev-parse --show-toplevel)/.git_hooks/ $(git rev-parse --show-toplevel)/.git/hooks
 STEM=`git rev-parse --show-toplevel`
 export STEM=$STEM
 module remove modulefile
